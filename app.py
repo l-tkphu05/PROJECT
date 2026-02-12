3d1a6ead86cc0f5022fb8d3e582746c0c8a1813d
l-tkphu05 <23133055@student.hcmute.edu.vn>
2026-02-12 16:15:15 +0700

    commit

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, jsonify
+import requests
+from typing import List
+
+app = Flask(__name__)
+
+# MODEL CLASSES
+class Company:
+    def __init__(self, id, name, address, zip_code, country,
+                 employee_count, industry, market_cap, domain, logo):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.zip_code = zip_code
+        self.country = country
+        self.employee_count = employee_count
+        self.industry = industry
+        self.market_cap = market_cap
+        self.domain = domain
+        self.logo = logo
+
+    def to_dict(self):
+        return self.__dict__
+
+
+class CompanyResponse:
+    def __init__(self):
+        self.companies: List[Company] = []
+
+    def add_company(self, company: Company):
+        self.companies.append(company)
+
+    def to_dict(self):
+        return {
+            "total": len(self.companies),
+            "companies": [c.to_dict() for c in self.companies]
+        }
+    
+
+# SERVICE CLASS
+class CompanyService:
+    URL = "https://fake-json-api.mock.beeceptor.com/companies"
+    def fetch_companies(self) -> CompanyResponse:
+        response = requests.get(self.URL)
+
+        if response.status_code != 200:
+            raise Exception("Failed to fetch data")
+
+        data = response.json()
+        result = CompanyResponse()
+
+        for item in data:
+            company = Company(
+                id=item.get("id"),
+                name=item.get("name"),
+                address=item.get("address"),
+                zip_code=item.get("zip"),
+                country=item.get("country"),
+                employee_count=item.get("employeeCount"),
+                industry=item.get("industry"),
+                market_cap=item.get("marketCap"),
+                domain=item.get("domain"),
+                logo=item.get("logo")
+            )
+            print(company.__dict__)
+            result.add_company(company)
+
+        return result
+
+
+# API ENDPOINT  
+@app.route("/companies", methods=["GET"])
+def get_companies():
+    service = CompanyService()
+    result = service.fetch_companies()
+    return jsonify(result.to_dict())
+
+# RUN SERVER
+if __name__ == "__main__":
+    app.run(debug=True)
